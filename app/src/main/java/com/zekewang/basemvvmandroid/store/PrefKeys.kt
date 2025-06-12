bf4c3ce1300df45b26c657e7d50c069b79025c7d
wz@dell <wz@dell.com>
2025-06-12 23:30:29 +0800

    feat:使用datastore保存用户信息和token数据

--- app/src/main/java/com/zekewang/basemvvmandroid/store/PrefKeys.kt
+++
@@ -1,7 +0,0 @@
-package com.zekewang.basemvvmandroid.store
-
-object PrefKeys {
-    const val TOKEN = "pref_token"
-    const val IS_FIRST_LAUNCH = "pref_is_first_launch"
-    const val USERNAME = "pref_username"
-}
